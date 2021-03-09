af8cafe80fb691d699ac40b86c1cf608cc8536e0
god19260 <68131294+god19260@users.noreply.github.com>
2021-03-09 00:36:54 -0600

    .

--- Proyecto_1.X/Proyecto_1.s
+++ Proyecto_1.X/Proyecto_1.s
@@ -1,3 +1,135 @@
+; Fredy Godoy 19260
+; Programación de Microcontroladores
+; Proyecto 1
+    
+processor 16F887
+#include <xc.inc>
+ 
+; CONFIG1
+  CONFIG  FOSC = INTRC_NOCLKOUT ; Oscillator Selection bits (INTOSCIO oscillator: I/O function on RA6/OSC2/CLKOUT pin, I/O function on RA7/OSC1/CLKIN)
+  CONFIG  WDTE = OFF             ; Watchdog Timer Enable bit (WDT enabled)
+  CONFIG  PWRTE = OFF           ; Power-up Timer Enable bit (PWRT disabled)
+  CONFIG  MCLRE = OFF           ; RE3/MCLR pin function select bit (RE3/MCLR pin function is MCLR)
+  CONFIG  CP = OFF              ; Code Protection bit (Program memory code protection is disabled)
+  CONFIG  CPD = OFF             ; Data Code Protection bit (Data memory code protection is disabled)
+  CONFIG  BOREN = OFF            ; Brown Out Reset Selection bits (BOR enabled)
+  CONFIG  IESO = OFF             ; Internal External Switchover bit (Internal/External Switchover mode is enabled)
+  CONFIG  FCMEN = OFF            ; Fail-Safe Clock Monitor Enabled bit (Fail-Safe Clock Monitor is enabled)
+  CONFIG  LVP = ON              ; Low Voltage Programming Enable bit (RB3/PGM pin has PGM function, low voltage programming enabled)
 
+; CONFIG2
+  CONFIG  BOR4V = BOR40V        ; Brown-out Reset Selection bit (Brown-out Reset set to 4.0V)
+  CONFIG  WRT = OFF             ; Flash Program Memory Self Write Enable bits (Write protection off)
 
+;---------------------------------------------------------
+;------------ Variables a usar----------------------------
+PSECT udata_shr  ; common memory  
 
+    #define B_Inc 0  ; pines puerto B, Incremento y Decremento
+    #define B_Dec 1
+    W_TEMP:         DS 1
+    STATUS_TEMP:    DS 1
+    
+;---------------------------------------------------------
+;------------ Reset Vector -------------------------------
+PSECT resVect, class=code, abs, delta=2  
+ORG 00h
+resVect:
+    PAGESEL main
+    goto    main 
+
+;---------------------------------------------------------
+;------------ Interrupción ---------------------------
+PSECT resVect, class=code, abs, delta=2  
+ORG 04h
+push:
+    movwf  W_TEMP
+    swapf  STATUS,W
+    movwf  STATUS_TEMP
+isr:
+    
+    btfsc  RBIF
+    ;Interrupcion Puerto B
+   
+    btfsc  T0IF
+    ;Interrupcion timer0
+
+    bcf    T0IF
+    BCF    RBIF
+pop: 
+    swapf  STATUS_TEMP,W
+    movwf  STATUS
+    swapf  W_TEMP, F
+    swapf  W_TEMP, W
+    RETFIE
+    
+;---------------------------------------------------------
+;------------ Definición del Inicio ----------------------
+PSECT code, delta=2, abs
+ORG 100h
+;---------------------------------------------------------
+;------------ Tablas -------------------------------------
+Display:
+    clrf  PCLATH
+    bsf   PCLATH,0
+    andlw 0x0F
+    addwf PCL
+    retlw 00111111B ; Cero
+    retlw 00000110B ;Uno
+    retlw 01011011B ;Dos
+    retlw 01001111B ;Tres
+    retlw 01100110B ;Cuatro
+    retlw 01101101B ;Cinco
+    retlw 01111101B ;Seis 
+    retlw 00000111B ;Siete
+    retlw 01111111B ;Ocho
+    retlw 01100111B ;Nueve
+    retlw 01110111B ;A
+    retlw 01111100B ;B
+    retlw 00111001B ;C
+    retlw 01011110B ;D
+    retlw 01111001B ;E
+    retlw 01110001B ;F 
+    
+;---------------------------------------------------------
+;------------ Main ---------------------------------------
+main: 
+    ;------- Configuraciones -------
+    ;------- Oscilador -------------
+    BANKSEL  OSCCON
+    bcf      IRCF0       ; Donfiguración del reloj interno 
+    bcf      IRCF1
+    bsf      IRCF2       ; 500khz
+    
+    ;------- Timer0 ---------------
+    bcf      OPTION_REG, 5
+    bcf      OPTION_REG, 3
+    bcf      OPTION_REG, 0     ; Se selecciona un preescaler de 128
+    bsf      OPTION_REG, 1
+    bsf      OPTION_REG, 2
+    
+    movlw    246       ; n de timer0
+    movwf    TMR0
+    ;------- Puertos ---------------
+    
+    ;------- Activación de pull ups
+    banksel  OPTION_REG
+    bcf      OPTION_REG, 7
+    bsf      WPUB, B_Inc    
+    bsf      WPUB, B_Dec 
+    
+    banksel  IOCB
+    bsf      IOCB, B_Inc   ; Habilitar Interrupt on change en B_Inc y B_Dec
+    bsf      IOCB, B_Dec  
+    
+    banksel  PORTA
+    movf     PORTB, W
+    
+    btfss    PORTB, 0      ; Primera instrucción que no genera interrupción
+    nop 
+
+;---------------------------------------------------------
+;----------- Loop Forever --------------------------------    
+loop:  
+    
+    goto     loop
